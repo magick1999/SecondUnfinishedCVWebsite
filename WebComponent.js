07aa816272558c54341e3e6309ba60aeb1b46c3e
unknown <976188@swansea.ac.uk>
2021-12-09 15:16:08 +0200

    Initialize project using Create React App

---
+++ WebComponent.js
@@ -0,0 +1,146 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const WebComponent = props => (
+  <svg {...props} width={1920} height={1080} viewBox="0 0 1920 1080">
+    <defs>
+      <style>
+        {
+          ".a{clip-path:url(#b);}.b,.j{fill:#fff;}.c,.e,.f,.h,.i{fill:#ee5c43;}.c,.j{font-size:16px;}.c,.f,.j{font-family:Montserrat-Bold, Montserrat;font-weight:700;}.d{fill:rgba(238,92,67,0.64);stroke:#707070;}.e{font-size:20px;font-family:Montserrat-Regular, Montserrat;}.f{font-size:40px;}.g{fill:none;}.h{fill-rule:evenodd;}.k{stroke:none;}.l{fill:#f0f0f0;}"
+        }
+      </style>
+      <clipPath id="b">
+        <rect width={1920} height={1080} />
+      </clipPath>
+    </defs>
+    <g id="a" className="a">
+      <rect className="l" width={1920} height={1080} />
+      <path className="b" d="M0,0H1366V900H0Z" transform="translate(277)" />
+      <g transform="translate(754.695 38)">
+        <text className="c" transform="translate(761.305 15)">
+          <tspan x={-35.664} y={0}>
+            {"RESUME"}
+          </tspan>
+        </text>
+        <text className="c" transform="translate(614.305 15)">
+          <tspan x={0} y={0}>
+            {"CONTACT"}
+          </tspan>
+        </text>
+        <text className="c" transform="translate(496.532 15)">
+          <tspan x={0} y={0}>
+            {"PROJECTS"}
+          </tspan>
+        </text>
+        <text className="c" transform="translate(409.482 15)">
+          <tspan x={0} y={0}>
+            {"SKILLS"}
+          </tspan>
+        </text>
+        <text className="c" transform="translate(212.624 15)">
+          <tspan x={0} y={0}>
+            {"WORK EXPERIENCE"}
+          </tspan>
+        </text>
+        <text className="c" transform="translate(81.821 15)">
+          <tspan x={0} y={0}>
+            {"EDUCATION"}
+          </tspan>
+        </text>
+        <text className="c" transform="translate(0 15)">
+          <tspan x={0} y={0}>
+            {"HOME"}
+          </tspan>
+        </text>
+      </g>
+      <g className="d" transform="translate(277 65)">
+        <rect className="k" width={683} height={835} rx={231} />
+        <rect
+          className="g"
+          x={0.5}
+          y={0.5}
+          width={682}
+          height={834}
+          rx={230.5}
+        />
+      </g>
+      <image
+        width={478}
+        height={535}
+        transform="translate(389 182)"
+        xlinkHref="ComponentTMP_0-image.png"
+      />
+      <g transform="translate(1052.497 364)">
+        <text className="e" transform="translate(0 66)">
+          <tspan x={3.47} y={19}>
+            {"Software developer passionate about delivering a "}
+          </tspan>
+          <tspan x={13.02} y={49}>
+            {"good customer experience, either through web "}
+          </tspan>
+          <tspan x={11.63} y={79}>
+            {"development, mobile development or full-stack "}
+          </tspan>
+          <tspan x={204.74} y={109}>
+            {"solutions"}
+          </tspan>
+        </text>
+        <text className="f" transform="translate(250 37)">
+          <tspan x={-197.38} y={0}>
+            {"Tian Bogdan-Mihai"}
+          </tspan>
+        </text>
+      </g>
+      <g transform="translate(1468 166)">
+        <g transform="translate(53 -18)">
+          <rect className="g" width={31} height={34} />
+          <path
+            className="h"
+            d="M91.522,34V18.511h5.289l.756-6.044H91.522V8.689c0-1.7.567-3.022,3.022-3.022h3.211V.189C97,.189,95.111,0,93.033,0,88.5,0,85.289,2.833,85.289,7.933v4.533H80v6.044h5.289V34Z"
+            transform="translate(-68.151 0.001)"
+          />
+        </g>
+        <g transform="translate(7 -18)">
+          <rect className="g" width={46} height={34} />
+          <path
+            className="h"
+            d="M50.094,32.978c14.428,0,22.279-11.882,22.279-22.279V9.638a17.247,17.247,0,0,0,3.819-4.031A17.613,17.613,0,0,1,71.736,6.88a8.255,8.255,0,0,0,3.395-4.244,19.461,19.461,0,0,1-4.88,1.91A7.578,7.578,0,0,0,64.522,2a7.972,7.972,0,0,0-7.851,7.851,4.136,4.136,0,0,0,.212,1.7A21.937,21.937,0,0,1,40.758,3.273,8.126,8.126,0,0,0,39.7,7.3a8.43,8.43,0,0,0,3.395,6.577,7.153,7.153,0,0,1-3.607-1.061h0a7.754,7.754,0,0,0,6.365,7.638,6.541,6.541,0,0,1-2.122.212,3.611,3.611,0,0,1-1.485-.212,8.038,8.038,0,0,0,7.426,5.517,16.02,16.02,0,0,1-9.76,3.395A5.874,5.874,0,0,1,38,29.159a20.027,20.027,0,0,0,12.094,3.819"
+            transform="translate(-38 -0.001)"
+          />
+        </g>
+        <g transform="translate(-44 -18)">
+          <path
+            className="h"
+            d="M17,3.022a52.126,52.126,0,0,1,6.8.189,8.761,8.761,0,0,1,3.211.567,6.634,6.634,0,0,1,3.211,3.211,8.761,8.761,0,0,1,.567,3.211c0,1.7.189,2.267.189,6.8a52.126,52.126,0,0,1-.189,6.8,8.761,8.761,0,0,1-.567,3.211,6.634,6.634,0,0,1-3.211,3.211,8.761,8.761,0,0,1-3.211.567c-1.7,0-2.267.189-6.8.189a52.126,52.126,0,0,1-6.8-.189,8.761,8.761,0,0,1-3.211-.567,6.634,6.634,0,0,1-3.211-3.211A8.761,8.761,0,0,1,3.211,23.8c0-1.7-.189-2.267-.189-6.8a52.126,52.126,0,0,1,.189-6.8,8.761,8.761,0,0,1,.567-3.211A6.784,6.784,0,0,1,5.1,5.1,3.194,3.194,0,0,1,6.989,3.778,8.761,8.761,0,0,1,10.2,3.211,52.126,52.126,0,0,1,17,3.022M17,0a55.811,55.811,0,0,0-6.989.189A11.661,11.661,0,0,0,5.856.944,7.394,7.394,0,0,0,2.833,2.833,7.394,7.394,0,0,0,.944,5.856a8.606,8.606,0,0,0-.756,4.156A55.811,55.811,0,0,0,0,17a55.811,55.811,0,0,0,.189,6.989,11.661,11.661,0,0,0,.756,4.156,7.394,7.394,0,0,0,1.889,3.022,7.394,7.394,0,0,0,3.022,1.889,11.661,11.661,0,0,0,4.156.756A55.811,55.811,0,0,0,17,34a55.812,55.812,0,0,0,6.989-.189,11.661,11.661,0,0,0,4.156-.756,7.925,7.925,0,0,0,4.911-4.911,11.661,11.661,0,0,0,.756-4.156C33.811,22.1,34,21.533,34,17a55.811,55.811,0,0,0-.189-6.989,11.661,11.661,0,0,0-.756-4.156,7.394,7.394,0,0,0-1.889-3.022A7.394,7.394,0,0,0,28.144.944,11.661,11.661,0,0,0,23.989.189,55.811,55.811,0,0,0,17,0m0,8.311A8.549,8.549,0,0,0,8.311,17,8.689,8.689,0,1,0,17,8.311m0,14.356A5.566,5.566,0,0,1,11.333,17,5.566,5.566,0,0,1,17,11.333,5.566,5.566,0,0,1,22.667,17,5.566,5.566,0,0,1,17,22.667M26.067,5.856a2.078,2.078,0,1,0,2.078,2.078,2.1,2.1,0,0,0-2.078-2.078"
+          />
+        </g>
+      </g>
+      <g transform="translate(294 16.533)">
+        <rect
+          className="i"
+          width={41}
+          height={28}
+          transform="translate(3 17)"
+        />
+        <text className="c" transform="translate(48.704 36.801)">
+          <tspan x={0} y={0}>
+            {"Mihai"}
+          </tspan>
+        </text>
+        <text className="j" transform="translate(6.475 36.467)">
+          <tspan x={0} y={0}>
+            {"M !="}
+          </tspan>
+        </text>
+      </g>
+    </g>
+  </svg>
+);
+
+export default WebComponent;
